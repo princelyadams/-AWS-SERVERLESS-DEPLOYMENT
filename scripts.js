d8bc029b95187dd297aa0e17f45da20742fc3cff
princelyadams <132752416+princelyadams@users.noreply.github.com>
2024-08-15 13:09:58 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
+var API_ENDPOINT = "https://1ve114pszc.execute-api.us-east-1.amazonaws.com/mystage";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
